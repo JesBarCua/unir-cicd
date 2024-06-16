d7a97fb00ca3e84fa4969217a8c222ecf6f74c4a
JesBarCua <jbarquers@alumnos.unex.es>
2024-06-16 04:03:28 +0200

    Jenkinsfile api test y e2e

---
+++ Jenkinsfile.apitest.e2e.groovy
@@ -0,0 +1,31 @@
+pipeline {
+    agent {
+        label 'docker'
+    }
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building stage!'
+                sh 'make build'
+            }
+        }
+        stage('API tests') {
+            steps {
+                sh 'make test-api'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
+        }
+        stage('e2e tests') {
+            steps {
+                sh 'make test-e2e'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
+        }
+    }
+    post {
+        always {
+            junit 'results/*_result.xml'
+            cleanWs()
+        }
+    }
+}
